0a96d12f8ce15eed5bc3cb7a218b68e921f9c0e0
kayp514 <pa.ka12@outlook.com>
2025-02-10 00:38:12 -0500

    Update middleware configuration and Tern Secure package
    - Refine public path matching for sign-in and sign-up routes
    - Adjust middleware matcher configuration
    - Upgrade @tern-secure/nextjs to version 4.2.7

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,19 @@
 import { ternSecureMiddleware, createRouteMatcher } from '@tern-secure/nextjs/server'
 
-//export const runtime = 'experimental-edge'
 
-const publicPaths = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+const publicPaths = createRouteMatcher([
+  '/sign-in',
+  '/sign-up'
+])
+
 
 
 export const config = {
     matcher: [
-      '/',
       '/((?!_next|__/auth|__/firebase|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-      '/api/:path*',
-      '/:id',
-    ]
-};
+      '/api/((?!auth).*)',
+    ],
+} 
 
 
 export default ternSecureMiddleware(async (auth, request) => {
