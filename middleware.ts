f449d776e1474c55d96f93bdc59ffededabca40b
kayp514 <pa.ka12@outlook.com>
2025-02-09 22:35:02 -0500

    Enhance middleware route matching and configuration
    - Expand public paths to include wildcard routes for sign-in and sign-up
    - Add additional route matchers for root, API, and user-specific paths
    - Refine middleware configuration to improve route protection and matching

--- middleware.ts
+++ middleware.ts
@@ -4,19 +4,24 @@ import { ternSecureMiddleware, createRouteMatcher } from '@tern-secure/nextjs/se
 export const runtime = 'experimental-edge'
 
 const publicPaths = createRouteMatcher([
-  '/sign-in',
-  '/sign-up',
+  '/sign-in/*',
+  '/sign-up/*',
   '/api/auth/*',
-  '/api/*'
+  '/api/*',
 ])
 
 
 export const config = {
     matcher: [
+      '/',
       '/((?!_next|__/auth|__/firebase|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-      '/api/((?!auth).*)'
-    ],
-} 
+      '/api/((?!auth).*)',
+      '/api/:path*',
+      '/:id',
+      '/sign-in',
+      '/sign-up'
+    ]
+};
 
 
 export default ternSecureMiddleware(async (auth, request) => {
